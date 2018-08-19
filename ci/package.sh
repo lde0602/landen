c90a3a2dbeae3429b4dd9d625a8167e2c2e6693d
lde0602 <lde0602@126.com>
2018-08-19 16:39:34 +0800

    update package.sh

--- ci/package.sh
+++ ci/package.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-tar -cf /data/landen_release/multi_table/multi_table.1.0.0 $basepath/../release/multi_table
+basepath=$(cd `dirname $0`; pwd)
+tar -cf /data/landen_release/multi_table/multi_table.1.0.0.tar $basepath/../release/multi_table
